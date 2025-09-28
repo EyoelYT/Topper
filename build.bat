2f3aecb73ab3a1a3ec0468fd3ffdc33bb8aa65c4
Eyoel Tesfu <EyoelYTesfu@gmail.com>
2025-09-28 17:01:54 -0500

    Use C++20

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
 rem call "c:/Program Files/Microsoft Visual Studio/2022/Community/VC/Auxiliary/Build/vcvarsall.bat" x64
 cl topper.cpp /std:c++17 -Zi User32.lib Dwmapi.lib Kernel32.lib /EHsc
+cl topper.cpp /std:c++20 -Zi User32.lib Dwmapi.lib Kernel32.lib /EHsc
